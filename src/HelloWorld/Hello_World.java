7c83d31d34564cccc55c728e11b76cbb1f0c63fc
TheRealDownloadTrump <128330477+TheRealDownloadTrump@users.noreply.github.com>
2023-08-29 18:52:53 -0500

    Create Hello_World.java

---
+++ src/HelloWorld/Hello_World.java
@@ -0,0 +1,33 @@
+public class Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello");
+        System.out.println("World!");
+        System.out.println("Hello");
+        System.out.println(" ");
+        System.out.println("World!");
+        System.out.println("**");
+        System.out.println("** Hello ");
+        System.out.println(" World ");
+        System.out.println("**");
+        System.out.print("Hello");
+        System.out.println("World!");
+        System.out.println("###");
+        System.out.println("# #");
+        System.out.println("###");
+        System.out.print("InsertNameHere,");
+        System.out.println("InsertNameHere");
+        System.out.println("InsertNameHere");
+        System.out.print("Something ");
+        System.out.print("Something ");
+        System.out.println("SOMETHING");
+        System.out.println("Java is\ncool\n!");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+}
