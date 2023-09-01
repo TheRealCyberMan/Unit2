6b6391ca8ac35ccec06bccba3c771f43f6e78f0e
TheRealDownloadTrump <128330477+TheRealDownloadTrump@users.noreply.github.com>
2023-09-01 16:07:40 -0500

    Create Salaries.java

---
+++ src/Salaries/Salaries.java
@@ -0,0 +1,29 @@
+public class Salaries {
+    public static void main(String[] args) {
+        // 1
+        String hi = "hi, ";
+        String mom = "Mom";
+        System.out.println(hi + mom);
+
+        // 2
+        String hi2 = "hi";
+        String mom2 = "Mom";
+        System.out.println(hi2 + ", " + mom2 + "!");
+
+        // 3
+        String someName = "Bob";
+        System.out.println("Hello, " + someName + "!");
+
+        // 4
+        String hello = "Hello";
+        String world = "World";
+        System.out.println(hello + ", " + world + "… Goodbye");
+
+         //5  Answer: Nothing
+
+        // 6
+        int a = 14;
+        int b = 6;
+        System.out.println(a + " % " + b + " is " + (a % b));
+    }
+}
