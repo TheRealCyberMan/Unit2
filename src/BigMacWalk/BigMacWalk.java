2e3ece701111a4ec51332cbc478a4d9f2d06895e
TheRealDownloadTrump <128330477+TheRealDownloadTrump@users.noreply.github.com>
2023-09-01 15:58:52 -0500

    Create BigMacWalk.java

---
+++ src/BigMacWalk/BigMacWalk.java
@@ -0,0 +1,58 @@
+public class BigMacWalk {
+    public static void main(String[] args) {
+        // 1
+        int apples;
+
+        // 2
+        apples = 12;
+
+        // 3
+        int oranges = 24;
+
+        // 4
+        System.out.println("You have this many apples " + apples);
+        System.out.println("You have this many oranges " + oranges);
+
+        // 5
+        double pi;
+        pi = 3.14159;
+        System.out.println("Pi is equal to " + pi);
+        
+		// add 6 here
+
+        // 7
+        String day = "Wednesday";
+
+        // 8
+        System.out.println("Hello! Today is " + day);
+
+        // 9
+        int a = 10;
+        int b = 20;
+
+        // 10
+        int c = b - a;
+        System.out.println("b minus a equals " + c);
+
+        // 11
+        int miles = 192;
+        int gallons = 16;
+        int mpg = 0;
+
+        // 12
+        mpg = miles / gallons;
+        System.out.println("Miles per gallon is " + mpg);
+
+        // 13
+        double length = 12.75;
+        double width = 8.65;
+        System.out.println("Area of rectangle is " + (length * width));
+
+        // add 14 here
+
+        // 15
+        double base = 11.89;
+        double height = 22.4;
+        System.out.println("Area of triangle is " + (0.5 * base * height));
+    }
+}
